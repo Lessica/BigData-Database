fdf7c99e40f229e47a8279ba29217ca2df530c5d
i_82 <i.82@qq.com>
2017-04-27 01:08:33 +0200

    Part 1.4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
+
+    /* PART 3 */
 
     // Exercise 1
     public static void displayDepartment(Connection connection) throws SQLException {
@@ -15,8 +18,8 @@ public class Main {
         resultSet.close();
     }
 
+    // Exercise 2
     public static void moveDepartment(Connection connection, int empno, int newDeptno) throws SQLException {
-        // Exercise 2
         Statement statement = connection.createStatement();
         statement.execute("UPDATE EMP SET DEPTNO = " + newDeptno + " WHERE EMPNO = " + empno);
     }
@@ -25,6 +28,16 @@ public class Main {
     public static void displayTable(Connection connection, String tableName) throws SQLException {
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery("SELECT * FROM " + tableName);
+        displayResultSet(resultSet);
+        resultSet.close();
+    }
+
+    // Exercise 4
+    // The way of execute raw sql statement cannot prevent leak caused by SQL injection.
+
+    /* PART 4 */
+
+    public static void displayResultSet(ResultSet resultSet) throws SQLException {
         ResultSetMetaData rsmd = resultSet.getMetaData();
         int columnsNumber = rsmd.getColumnCount();
         for (int i = 1; i <= columnsNumber; i++) {
@@ -37,11 +50,40 @@ public class Main {
             }
             System.out.println();
         }
-        resultSet.close();
     }
 
-    // Exercise 4
-    // The way of execute raw sql statement cannot prevent leak caused by SQL injection.
+    public static void searchEmployer(Connection connection) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "SELECT * FROM EMP WHERE EFIRST = ? AND ENAME = ?"
+        );
+        System.out.print("Employer Name: ");
+        Scanner sc = new Scanner(System.in);
+        String firstName = sc.next();
+        preparedStatement.setString(1, firstName);
+        String lastName = sc.next();
+        preparedStatement.setString(2, lastName);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        displayResultSet(resultSet);
+    }
+
+    // Exercise 5
+    public static void moveDepartmentSafe(Connection connection, int empno, int newDeptno) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "UPDATE EMP SET DEPTNO = ? WHERE EMPNO = ?"
+        );
+        preparedStatement.setInt(1, newDeptno);
+        preparedStatement.setInt(2, empno);
+        preparedStatement.execute();
+    }
+
+    // Exercise 6
+    // You can't.
+    // You need to construct the sql with string concatenation/placeholder with String.format.
+    // Prepared statement is for the column values not for table name.
+    // It is rather easy to check the table name against the list of available tables in the database.
+
+    // Exercise 7
+    // You may better check it in WEB Technologies TPs.
 
     public static void main(String[] args) {
         /* Load JDBC Driver. */
@@ -59,9 +101,13 @@ public class Main {
             connection = DriverManager.getConnection(url, user, pass);
             // Requests to bdd will be here
             System.out.println("Bdd Connected");
+
             displayDepartment(connection);
             moveDepartment(connection, 7584, 10);
             displayTable(connection, "EMP");
+            searchEmployer(connection);
+            moveDepartmentSafe(connection, 7584,20);
+
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
